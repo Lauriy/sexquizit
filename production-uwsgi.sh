194000431b80ae39be2bc9788585eebeda0cb4be
Lauri Elias <laurileet@gmail.com>
2016-12-12 21:23:21 +0200

    Update live run command

--- production-uwsgi.sh
+++ production-uwsgi.sh
@@ -3,5 +3,5 @@ export HOME=/home/sexquizit
 cd $HOME
 source venv/bin/activate
 cd $HOME/want-will-wont/
-uwsgi --socket /home/sexquizit/want-will-wont/sexquizit.sock --module want_will_wont.wsgi --chmod-socket=777 --env=LANG="en_US.utf8"
+uwsgi --socket /home/sexquizit/want-will-wont/sexquizit.sock --processes=1 --module want_will_wont.wsgi --chmod-socket=777 --env=LANG="en_US.utf8"
 exit $?
