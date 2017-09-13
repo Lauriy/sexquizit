1bc5cf6765ec4d3b05bca89d199110e2ebd3e1bc
Lauri Elias <laurileet@gmail.com>
2017-09-13 11:24:19 +0300

    Prepare for Github

--- want_will_wont/settings/base.py
+++ want_will_wont/settings/base.py
@@ -73,8 +73,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DATABASE_NAME', 'wantwillwont_dev'),
         'USER': os.getenv('DATABASE_USER', 'wantwillwont'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'd26ec1805f28f3af6d51c6c2b72bc005'),
-        'HOST': os.getenv('DATABASE_HOST', '46.101.238.121'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', ''),
+        'HOST': os.getenv('DATABASE_HOST', ''),
         'PORT': os.getenv('DATABASE_PORT', ''),
     }
 }
